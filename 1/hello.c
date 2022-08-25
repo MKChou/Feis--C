43fe713b1ab72ea6c43a041c28864a3d6b67646a
mkchou <AN4096750@gs.ncku.edu.tw>
2022-08-26 00:41:01 +0800

    1.1

--- 1/hello.c
+++ 1/hello.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World! \n");
+    printf("Hello World!\n");
+    printf("Hello ");
+    printf("World!");
     return 0;
 }
 
 
+"""
 
-echo "# Feis--C" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add aaa git@github.com:MKChou/Feis--C.git
-git push -u aaa main
+跳脫字元 \n \" \" 
+
+"""
