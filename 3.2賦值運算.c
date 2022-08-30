fc2de4a9f93aeec3b4e0dee2d019b90d19e57535
mkchou <AN4096750@gs.ncku.edu.tw>
2022-08-30 23:46:55 +0800

    3.2

---
+++ 3.2賦值運算.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a,c;
+    double b,d;
+    a = b = c = d = 3+7/2.;
+    printf("a=%d\n",a);
+    printf("b=%f\n",b);
+    printf("c=%d\n",c);
+    printf("d=%f\n",d);
+}
