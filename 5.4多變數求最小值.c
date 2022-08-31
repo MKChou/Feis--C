cf0a10c90b65a53c91bc74f0bd32df0a76b9300e
mkchou <AN4096750@gs.ncku.edu.tw>
2022-09-01 01:34:12 +0800

    220901 01:34:08

---
+++ 5.4多變數求最小值.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, min;
+    printf("Please enter 2 number:");
+    scanf("%d%d%d", &a, &b, &c);
+    min = a;
+    if (min > b)
+    {
+        min = b;
+    }
+    if (min > c)
+    {
+        min = c;
+    }
+
+    printf("Min:%d", min);
+    return 0;
+}
