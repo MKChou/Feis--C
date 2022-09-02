77a2bd173e886498f08843790fa2b58ec32767a4
mkchou <AN4096750@gs.ncku.edu.tw>
2022-09-03 00:00:31 +0800

    220903 00:00:27

---
+++ 6.2三變數數值交換.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c,t;
+
+    printf("ABC>CBA\n");
+    printf("Enter 3 number:");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("before:%d %d %d\n",a,b,c);
+    t = a;
+    a = c;
+    c = b;
+    b = t;
+
+
+    printf("after:%d %d %d",a,b,c);
+}
