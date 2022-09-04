2108496eb86a7f4b1801a35fd85d09e134748d76
mkchou <AN4096750@gs.ncku.edu.tw>
2022-09-04 22:03:48 +0800

    220904 22:03:44

---
+++ 8.1猜數字練習.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int guess;
+    int number = 8;
+
+    printf("Guess a number(1~9):");
+    scanf("%d",&guess);
+
+    while(guess!=number){
+        if(guess>number){
+            printf("Too large!\n");
+        }
+        else{
+            printf("Too small!\n");
+        }
+        printf("Guess a number(1~9):");
+        scanf("%d",&guess);
+    }
+
+    printf("Bingo!");
+}
