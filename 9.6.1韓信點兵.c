1e67d4a98c4ddfb4904723ff8b86de28d1dc409e
mkchou <AN4096750@gs.ncku.edu.tw>
2022-09-05 15:14:14 +0800

    2202905 15:14:04

---
+++ 9.6.1韓信點兵.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int max;
+    int number;
+    int answer=0;
+
+    printf("Enter:");
+    scanf("%d",&max);
+
+    for ( number=max; number>=0 && answer==0; number--){
+        if(number%3==2 && number%5==3 && number%7==2){
+            break;
+        }
+    }
+
+    printf("Max:%d",number);
+    return 0;
+}
