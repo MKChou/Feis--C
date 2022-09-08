51c90a25626354140dc6ed849d7fd820371423b7
mkchou <AN4096750@gs.ncku.edu.tw>
2022-09-08 20:35:49 +0800

    220908 20:35:45

--- 15.5.0畫數字統計長條圖.c
+++ 15.5.0畫數字統計長條圖.c
@@ -1,11 +1,25 @@
-XX
+#include <stdio.h>
 
 
+int main(){
+    int i,j,n,b[10]={0};
 
+    for ( i=0; i<10; i++){
+        printf("Enter: ");
+        scanf("%d",&n);
+        b[(n)/10]++;
+    }
 
+    
+    for ( i=0; i<10; i++){
+        printf("%d0:",i);
 
+        for ( j=0; j<b[i]; j++){
+            printf("*");
+        }
 
+        printf("\n");
 
-
+        return 0;
+    }
 }
-
