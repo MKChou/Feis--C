e1e0f3b559cb707e51717d56a71d9ddf7512febd
mkchou <AN4096750@gs.ncku.edu.tw>
2022-09-09 02:45:25 +0800

    220909 02:45:16

---
+++ 17.3.0泡沫排序法.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(){
+    int i,j,number[5]={0};
+
+    for ( j=0; j<5; j++){
+        printf("Enter a number:");
+        scanf("%d",&number[j]);
+    }
+    
+
+
+    for ( j=0; j<4; j++){
+        for( i=0; i<4-j; i++){
+            if( number[i] > number[i+1] ){
+                int t =number[i];
+                number[i] = number[i+i];
+                number[i+1]=t;
+            }
+        }
+    }
+
+    for (j=0; j<5; j++){
+        printf("%d ",number[j]);
+    }
+    return 0;
+}
