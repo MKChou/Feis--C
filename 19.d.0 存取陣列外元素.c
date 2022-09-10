181096d9d24fa8b9e56abb3cd9520887e3d114c5
mkchou <AN4096750@gs.ncku.edu.tw>
2022-09-10 17:45:03 +0800

    220910 17:44:59

--- 19.d.0 存取陣列外元素.c
+++ 19.d.0 存取陣列外元素.c
@@ -7,12 +7,12 @@ int main(){
     printf("%d\n",v[0]);
     printf("%d\n",v[1]);
     printf("%d\n",v[2]);
-    printf("%d\n",v[3/2]);  // v[1]
+    printf("%d\n",v[3/2]);   // v[1]
 
-    // printf("%d",0.5);    // (X)編譯錯誤
-    //printf("%d",3/2.");   // (X)編譯錯誤 
+    // printf("%d",0.5);     // (X)編譯錯誤
+    //printf("%d",3/2.");    // (X)編譯錯誤 
     
-    printf("%d",v[3]);      // (X)未定義行為
+    printf("%d",v[3]);       // (X)未定義行為
 
     return 0;
 }
